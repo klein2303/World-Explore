2e6351d937cc8b1f598ae078c0f855d004df4ad0
Alice <alicezheng888@gmail.com>
2024-10-02 23:43:23 +0200

    feat(#5): create mock data

---
+++ frontend/src/types/CountryTypes.ts
@@ -0,0 +1,13 @@
+export type CountryType = { 
+    name: string,
+    continent: string,
+    capital: string,
+    largest_city: string,
+    currency: string,
+    language: string,
+    population: number,
+    land_area: number,
+    agriculture_area: string,
+    forest_area: string,
+    co2_emissions: number,
+}

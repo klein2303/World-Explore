25616c25ec5aca7092feeebec3619e427aa572c4
Alice <alicezheng888@gmail.com>
2024-10-03 13:47:07 +0200

    refactor(#5): add functioncality

--- frontend/src/components/CountryCard/CountryCard.tsx
+++ frontend/src/components/CountryCard/CountryCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import styles from './CountryCard.module.css'
 import {LuMapPin} from "react-icons/lu";
 
@@ -7,12 +8,31 @@ interface CountryCardProps {
 }
 
 const CountryCard = ({name, image}: CountryCardProps) =>  {
+    const [isFilled, setIsFilled] = useState<boolean>(false);
+    
+    const fillMapPin = () => {
+        setIsFilled(!isFilled);
+
+        if (isFilled) {
+            localStorage.removeItem(name);
+        } else {
+            localStorage.setItem(name, 'filled');
+        }
+    }
+
+    useEffect(() => {
+        const filled = localStorage.getItem(name);
+        if (filled) {
+            setIsFilled(true);
+        }
+    }, [name])
+
     return (
         <div className={styles.cardcontainer}>
             <img src={image} alt={`image of the country ${name}`} className={styles.cardimage} />
             <div className={styles.information}>
-                <p>{name}</p>
-                <LuMapPin />
+                <p className={styles.name}>{name}</p>
+                {isFilled ? <LuMapPin onClick={fillMapPin} className={styles.filled}/> : <LuMapPin onClick={fillMapPin} className={styles.icon}/>}
             </div>
         </div>
     );

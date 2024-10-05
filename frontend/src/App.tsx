18f858c6e2949b121a324260f0af7558e810d3e1
Alice <alicezheng888@gmail.com>
2024-10-06 01:01:54 +0200

    refactor(#8): change styling for country card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import Router from "./Router";
+import { RecoilRoot } from "recoil";
 
 function App() {
     return (
-        <>
+        <RecoilRoot>  
             <Router />
-        </>
+        </RecoilRoot>
     );
 }
 

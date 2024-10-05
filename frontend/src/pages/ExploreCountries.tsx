654c5bfc8750efdf3c82b90107d7ffab7f916c1e
Alice <alicezheng888@gmail.com>
2024-10-06 01:09:00 +0200

    refactor(#8): update pages based on new code

--- frontend/src/pages/ExploreCountries.tsx
+++ frontend/src/pages/ExploreCountries.tsx
@@ -1,9 +1,43 @@
 import Navbar from "../components/Navbar/Navbar";
+import CountryCardList from "../components/CountryCard/CountryCardList";
+import Countrys from "../data/Countrys";
+import Filter from "../components/Filter/Filter";
+import styles from "../styles/ExploreCountries.module.css";
+import Search from "../components/Search/Search";
+import { useEffect, useState } from "react";
+import { filterAtom } from "../atoms/FilterAtom";
+import { useRecoilValue } from "recoil";
 
 const ExploreCountries = () => {
+    const [countries, setCountries] = useState(Countrys);
+    const filter = useRecoilValue(filterAtom);
+    useEffect(() => {
+        // const filters = sessionStorage.getItem("clickedContinents") ? JSON.parse(sessionStorage.getItem("clickedContinents") as string) : [];
+        // if (!sessionStorage.getItem("clickedContinents")) {
+        //     sessionStorage.setItem("clickedContinents", "[]");
+        // }
+
+        if (!Object.values(filter.continent).includes(true)) {
+            setCountries(Countrys);
+            setCountries((prevCountries) => prevCountries.filter((country) => country.name.toLowerCase().includes(filter.search.toLowerCase())));
+        }
+        else {
+            setCountries(Countrys.filter((country) => filter.continent[country.continent as keyof typeof filter.continent] === true));
+            setCountries((prevCountries) => prevCountries.filter((country) => country.name.toLowerCase().includes(filter.search.toLowerCase())));
+        }
+
+    }, [Countrys, filter]);
+
     return (
         <>
             <Navbar />
+            <main className={styles.maincontainer}>
+                <Filter />
+                <div>
+                    <Search />
+                    <CountryCardList countries={countries} />
+                </div>
+            </main>
         </>
     );
 };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,9 @@
 import Navbar from "../components/Navbar/Navbar";
-import CountryCardList from "../components/CountryCard/CountryCardList";
-import Countrys from "../data/Countrys";
 
 const Home = () => {
     return (
         <>
             <Navbar />
-            <CountryCardList countries={Countrys} />
         </>
     );
 };

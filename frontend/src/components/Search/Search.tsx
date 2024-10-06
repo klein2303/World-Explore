3ce2264153686357e59c655bfc35a40b95b455b8
Alice <alicezheng888@gmail.com>
2024-10-07 01:26:53 +0200

    refactor(#2): fix linting warnings

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -2,36 +2,34 @@ import styles from "./Search.module.css";
 import {GoSearch} from "react-icons/go";
 import { filterAtom } from "../../atoms/FilterAtom";
 import { useRecoilState } from "recoil";
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import { FilterType } from "../../types/FilterType";
 import { setFilters, getFilters} from "../../utils/FilterStorage";
 import { useDebounce } from 'use-debounce';
 
+
 const Search = () => {
-    const [search, setSearch] = useRecoilState<FilterType>(filterAtom);
+    const [filter, setFilter] = useRecoilState<FilterType>(filterAtom);
     const [input, setInput] = useState<string>(getFilters().search);
     const [debouncedSearch] = useDebounce(input, 1000);
 
-    const handleSearch = () => {
-        setSearch((prevState) => ({
-            ...prevState,
-            search: input,
-        }));
-    };
-
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInput(e.target.value);
+        setInput(e.target.value);  
     }
 
     useEffect(() => {
-        handleSearch();
-        setFilters(search);
-    }, [debouncedSearch]);
+        if (debouncedSearch == filter.search) return;
+        setFilter((prevState) => ({
+            ...prevState,
+            search: debouncedSearch,
+        }));
+        setFilters(filter);
+    }, [debouncedSearch, setFilter, filter]);
 
     return (
         <main className={styles.searchcontainer}>
             <input type="text" placeholder="Search after countries..." className={styles.input} aria-label="Search countries" onChange={handleInputChange} value={input}/>
-            <GoSearch className={styles.searchicon} onClick={handleSearch} role="button" aria-label="Click to search" tabIndex={0}/>
+            <GoSearch className={styles.searchicon}  role="button" aria-label="Click to search" tabIndex={0}/>
         </main>
     );
 };

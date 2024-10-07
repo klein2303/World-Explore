2d86253107ac6ad6297b598b535e9b4852d8ef11
nathanim <nathanim@stud.ntnu.no>
2024-10-07 13:56:08 +0200

    feat(#12): Add logo and links to login page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,18 +1,32 @@
 import LoginOrRegister from "../components/LoginOrRegister/LoginOrRegister";
 import styles from "../styles/Register.module.css";
-import loginImage from "frontend/public/loginSunset.jpg";
+import loginImage from "frontend/public/loginImagePurple.jpg";
+import {Link} from "react-router-dom";
 
 const Register = () => {
     return ( 
     <div className={styles.registerPage}>
         <section className={styles.inputSection}>
+            <section className={styles.headerRegisterPage}>
+                <section className={styles.logo}>
+                    <p>WorldExplore</p>
+                </section>
+                <section className={styles.linkToLogIn}>
+                    <Link to={"/LogIn"} className={styles.navlinkOver}>
+                        <p>Already registered?</p>
+                    </Link>
+                </section>
+            </section>
             <LoginOrRegister/>
+            <section className={styles.linkToLogInUnder}>
+                <Link to={"/LogIn"} className={styles.navlinkUnder}>
+                    <p>Already registered?</p>
+                </Link>
+            </section>
         </section>
         <section className={styles.image}>
             <img src= {loginImage} alt="A beautiful landscape" width="700" height="700"></img>
-            
         </section>
-
     </div>
 
     );

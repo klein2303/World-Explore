9fdb0b15739ad29b2383618b9cb918f8cf2f52e0
Kleinc <kleinc@stud.ntnu.no>
2024-10-10 18:55:45 +0200

    refactor(#16): fix missing routing after merge conflict

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -6,6 +6,7 @@ import MyJournals from "./pages/MyJournals";
 import LogIn from "./pages/LogIn";
 import Register from "./pages/Register";
 import JournalPage from "./pages/JournalPage";
+import Country from "./pages/Country";
 
 const Router = () => {
     return (
@@ -25,6 +26,7 @@ const Router = () => {
                 <Route path="/ExploreCountries" element={<ExploreCountries />} />
                 <Route path="/MyJournals" element={<MyJournals />} /> 
                 <Route path="/JournalPage/:countryName" element={<JournalPage />} />
+                <Route path="/:name" element={<Country />} />
             </Routes>
         </BrowserRouter>
     );

feddc529a3c3fc517461e48cc5cefce38105db1f
Alice <alicezheng888@gmail.com>
2024-12-03 10:19:23 +0100

    refactor(#131): refactor the journal page to show 404 when the country doesn't exist

--- frontend/src/components/ReviewBox/ReviewBox.tsx
+++ frontend/src/components/ReviewBox/ReviewBox.tsx
@@ -2,6 +2,8 @@ import styles from "./ReviewBox.module.css";
 import { FaRegStar, FaStar } from "react-icons/fa";
 import { gql, useQuery } from "@apollo/client";
 import { removeQuotes } from "../../utils/utils";
+import { PiArrowElbowDownLeft } from "react-icons/pi";
+import { Link, useParams } from "react-router-dom";
 
 interface journalCountry {
     country: string;
@@ -9,6 +11,7 @@ interface journalCountry {
 
 const ReviewBox = ({ country }: journalCountry) => {
     const user = removeQuotes(sessionStorage.getItem("user")!);
+
     const JOURNAL = gql`
         query GetJournal($countryid: String!, $profileid: String!) {
             writtenjournal(countryid: $countryid, profileid: $profileid) {
@@ -44,35 +47,54 @@ const ReviewBox = ({ country }: journalCountry) => {
     if (error) return <p>Error: {error.message}</p>;
     return (
         <main className="main">
-            {data.writtenjournal
-                ? data.writtenjournal.reviews.map(
-                      (review: {
-                          id: number;
-                          title: string;
-                          date: string;
-                          text: string;
-                          rating: number;
-                          ispublic: boolean;
-                      }) => (
-                          <section
-                              key={review.id}
-                              className={styles.reviewBox}
-                              aria-description="Section containing the journal entry"
-                              tabIndex={0}>
-                              <p className={styles.reviewTitle}>{review.title}</p>
-                              <p className={styles.date}>{review.date}</p>
-                              <article className={styles.reviewText}>{review.text} </article>
-                              <section className={styles.starsContainer} aria-label="rating">
-                                  {/* Render stars based on rating */}
-                                  {renderStars(review.rating)}
-                              </section>
-                              <p className={styles.publicInfoText}>
-                                  This journal entry is {review.ispublic ? "public" : "private"}
-                              </p>
-                          </section>
-                      ),
-                  )
-                : null}
+            {data.writtenjournal ?
+                (<>
+                    <section>
+                        <Link to={"/MyJournals"} className={styles.returnLink}>
+                            {" "}
+                            <PiArrowElbowDownLeft /> Return to all journals
+                        </Link>
+                        <section className={styles.upperSection}>
+                            <p className={styles.title} aria-label="journals">
+                                My {country} journals
+                            </p>
+                        </section>
+                    </section>
+                    {data.writtenjournal.reviews.map(
+                        (review: {
+                            id: number;
+                            title: string;
+                            date: string;
+                            text: string;
+                            rating: number;
+                            ispublic: boolean;
+                        }) => (
+                            <section
+                                key={review.id}
+                                className={styles.reviewBox}
+                                aria-description="Section containing the journal entry"
+                                tabIndex={0}>
+                                <p className={styles.reviewTitle}>{review.title}</p>
+                                <p className={styles.date}>{review.date}</p>
+                                <article className={styles.reviewText}>{review.text} </article>
+                                <section className={styles.starsContainer} aria-label="rating">
+                                    {/* Render stars based on rating */}
+                                    {renderStars(review.rating)}
+                                </section>
+                                <p className={styles.publicInfoText}>
+                                    This journal entry is {review.ispublic ? "public" : "private"}
+                                </p>
+                            </section>
+                        ))}
+                    </>
+                    ) : (
+                    <main className={styles.noCountry} role="main" aria-describedby="not-found">
+                        <h1 id="not-found" aria-label="Error 404: Page not found">
+                            404
+                        </h1>
+                    </main>
+                )
+            }
         </main>
     );
 };
--- frontend/src/pages/JournalPage.tsx
+++ frontend/src/pages/JournalPage.tsx
@@ -1,11 +1,8 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
-import { PiArrowElbowDownLeft } from "react-icons/pi";
 import styles from "../styles/JournalPage.module.css";
 import ReviewBox from "../components/ReviewBox/ReviewBox";
-import { useEffect, useState } from "react";
-import JournalEntryModal from "../components/JournalEntryModal/JournalEntryModal"; // Import the modal component
-import { JournalTypeWrite } from "../types/JournalType"; // Import your JournalType
+import { useEffect } from "react";
 
 const JournalPage = () => {
     // Get countryName from the URL
@@ -13,27 +10,6 @@ const JournalPage = () => {
 
     // Let country be an default string if countryName is empty
     const country = countryName || "";
-
-    // State to manage the modal visibility
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    // Function to open the modal
-    // const openModal = () => {
-    //     setIsModalOpen(true);
-    // };
-
-    // Function to close the modal
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
-
-    // Handle submission of journal entry
-    const handleJournalSubmit = (entry: JournalTypeWrite) => {
-        console.log("New journal entry submitted:", entry);
-        // Add your logic here to save the journal entry
-        // Then close the modal
-        closeModal();
-    };
 
     // Scroll to top when the component mounts
     useEffect(() => {
@@ -44,26 +20,7 @@ const JournalPage = () => {
         <>
             <Navbar />
             <main className={styles.page} aria-description="Page for reading your journals">
-                <Link to={"/MyJournals"} className={styles.returnLink}>
-                    {" "}
-                    <PiArrowElbowDownLeft /> Return to all journals
-                </Link>
-                <section className={styles.upperSection}>
-                    <p className={styles.title} aria-label="journals">
-                        My {countryName} journals
-                    </p>
-                    {/* <button className={styles.addButton} aria-label="Add new journal entry" onClick={openModal}>
-                        Add new journal entry <SlPencil />
-                    </button> */}
-                </section>
                 <ReviewBox country={country} />
-                {/* Render the JournalEntryModal */}
-                <JournalEntryModal
-                    country={country}
-                    isOpen={isModalOpen}
-                    onClose={closeModal}
-                    onSubmit={handleJournalSubmit}
-                />
             </main>
         </>
     );
